8ad254a7dbebbe662004118324db8b0c7cdcf6e6
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-10-21 15:54:50 -0400

    refactoring + moving card view styling out of vc + grouping files into categories + generalizing tap button action + altering swipe actions to recognize when cards are face up + removing random color generators + adding launchscreen

---
+++ tarotReader/App Lifecycle/ViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ViewController.swift
+//  tarotReader
+//
+//  Created by Marty Hernandez Avedon on 10/20/17.
+//  Copyright © 2017 Marty's . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var card1: UIImageView!
+    @IBOutlet weak var card2: UIImageView!
+    @IBOutlet weak var card3: UIImageView!
+    
+    var manager: APIRequestManager!
+    var cardDisplayer: CardDisplayer!
+    var card: TarotCard!
+    var cards: [TarotCard]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.manager = APIRequestManager()
+        self.cardDisplayer = CardDisplayer()
+        self.cards = []
+        
+        let indices = cardDisplayer.generateThreeRandomIndices()
+        
+        grabCard(at: indices[0])
+        grabCard(at: indices[1])
+        grabCard(at: indices[2])
+        
+        cardDisplayer.shade(views: [self.card1, self.card2, self.card3])
+    }
+    
+    func grabCard(at index: Int) {
+        let cardURL = "\(Endpoint.get)=\(index)"
+        
+        self.manager.getData(endPoint: cardURL) { (data: Data?) in
+            if let validData = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: validData, options: [])
+                    
+                    guard let results = json as? [String: Any],
+                        let response = results["response"] as? [String: AnyObject] else {
+                            throw Serialized.error
+                    }
+                    
+                    if let card = TarotCard(dict: response) {
+                        self.cards.append(card)
+                        print(card.title)
+                    }
+                    
+                }
+                catch {
+                    print("error making get request")
+                }
+            }
+        }
+    }
+    
+    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
+        let cardsFaceUp = [self.card1, self.card2, self.card3].filter{ $0?.image != nil }
+        
+        guard cardsFaceUp.isEmpty else {
+            _ = cardsFaceUp.map { $0?.image = nil }
+            return
+        }
+        
+        let firstCardColor = self.card1.backgroundColor
+        let secondCardColor = self.card2.backgroundColor
+        let thirdCardColor = self.card3.backgroundColor
+        
+        self.card1.backgroundColor = thirdCardColor
+        self.card2.backgroundColor = firstCardColor
+        self.card3.backgroundColor = secondCardColor
+    }
+    
+    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        DispatchQueue.main.async {
+            let cardsFaceUp = [self.card1, self.card2, self.card3].filter{ $0?.image != nil }
+            
+            guard cardsFaceUp.isEmpty else {
+                _ = cardsFaceUp.map { $0?.image = nil }
+                return
+            }
+            
+            let firstCardColor = self.card1.backgroundColor
+            let secondCardColor = self.card2.backgroundColor
+            let thirdCardColor = self.card3.backgroundColor
+            
+            self.card1.backgroundColor = secondCardColor
+            self.card2.backgroundColor = thirdCardColor
+            self.card3.backgroundColor = firstCardColor
+        }
+    }
+    
+    @IBAction func cardWasTapped(_ sender: UITapGestureRecognizer) {
+        guard let tag = sender.view?.tag else { return }
+        var card: UIImageView
+        
+        switch tag {
+        case 0:
+            card = self.card1
+        case 1:
+            card = self.card2
+        case 2:
+            card = self.card3
+        default:
+            return
+        }
+        
+        guard card.image == nil else { return }
+        
+        DispatchQueue.main.async {
+            guard self.cards.count > tag else { return }
+            if let cardFace = self.cardDisplayer.makeCardFace(from: self.cards[tag].imageAddress) {
+                card.image = cardFace
+            }
+        }
+    }
+    
+}
+
---
+++ tarotReader/Visuals/CardDisplayer.swift
@@ -0,0 +1,59 @@
+//
+//  CardCreation.swift
+//  tarotReader
+//
+//  Created by Marty Hernandez Avedon on 10/21/17.
+//  Copyright © 2017 Marty's . All rights reserved.
+//
+
+import UIKit
+
+struct CardDisplayer {
+    init() {}
+    
+    func generateThreeRandomIndices() -> [Int] {
+        let deckSize: UInt32 = 3
+        var randomCardIndexes: [Int] = []
+        
+        while randomCardIndexes.count < 3 {
+            let freshIndex = arc4random_uniform(deckSize)
+            
+            guard !randomCardIndexes.contains(Int(freshIndex)) else {
+                continue
+            }
+            
+            randomCardIndexes.append(Int(freshIndex))
+        }
+        
+        return randomCardIndexes
+    }
+    
+    func shade(views: [UIView]) {
+        for view in views {
+            view.layer.shadowColor = UIColor.darkGray.cgColor
+            view.layer.shadowOpacity = 1
+            view.layer.shadowOffset = CGSize(width: 0, height: 1)
+            view.layer.shadowRadius = 3
+            view.layer.shadowPath = UIBezierPath(rect: view.bounds).cgPath
+            view.layer.shouldRasterize = false
+        }
+    }
+    
+    func makeCardFace(from url: URL) -> UIImage? {
+        var image: UIImage
+        
+        do {
+            let data: Data? = try Data(contentsOf: url)
+            
+            if let validImage =  UIImage(data: data!) {
+                image = validImage
+                return image
+            }
+        }
+        catch {
+            print("error creating image from \(url)")
+        }
+        
+        return nil
+    }
+}
